f525d8400e0a616e221f437ebab8779e57fec921
Bowencn <ousttiiaa@outlook.com>
2019-05-04 15:37:09 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div class="background">
+      <img :src="backImg" class=".img-responsive">
+    </div> -->
+    <div class="main">
+      <div class="content">
+        
+      </div>
+    </div>
+    <slideBar class="Sidebar"></slideBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import backgroundImg from "./assets/18.jpg";
+import slideBar from "./components/slideBar";
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      backImg:backgroundImg
+    }
+  },
+  components:{
+    slideBar:slideBar
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('./assets/reset.css');
+html{
+  height: 100%;
+  &::-webkit-scrollbar { width: 0 !important }
+  body{
+    height: 100%;
+    text-align: center;
+    background: url('./assets/18.jpg');
+    background-size: 100%;
+      #app{
+        height: 100%;
+        width: 100%;
+        // position: relative;
+        .background{
+          position: relative;
+          font-size: 0;
+        }
+        .main{
+          width: 100vw;
+          height: 100vh;
+          // margin-left: 10vh;
+          .content{
+            // border: 1px solid black;
+            margin: 0 auto;
+            background-color: rgba(255,255,255,.5);
+            width: 60vw;
+            height: 100vh;
+          }
+        }
+        .Sidebar{
+          position: fixed;
+          top: 20vh;
+          background-color: rgba(0, 0, 0, 0.8);
+          border-right: 1px solid rgba(0, 0, 0, 0.5);
+          box-shadow: 1vh 1vh 2vh 0vh rgba(0, 0, 0, 0.6);
+          width: 10vh;
+          // height: 100vh;
+        }
+    }
+  }
 }
 </style>
---
+++ src/components/slideBar.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container bar-main">
+    <ul class="column main-ul">
+      <li class="userImg">
+        <img src="../assets/userimg.jpg" class="">
+        <p>{{userName}}</p>
+      </li>
+      <li class="log">
+        <span>{{logNum}}</span>
+        <P>日志</P>
+      </li>
+      <li class="log">
+        <span>{{projectNum}}</span>
+        <P>项目</P>
+      </li>
+      <li>
+        <a href="" class="github">
+          <img src="../assets/github.svg" class="github-img">
+          <span></span>
+          <p>Github</p>
+        </a>
+      </li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      userName:'Chris',
+      logNum:'10',
+      projectNum:'1'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .bar-main{
+    .main-ul{
+      width: 100%;
+      height: 100%;
+      li{
+        cursor: pointer;
+        margin-top: 5vh;
+        position: relative;
+        .github{
+          height: 10vh;
+          span{
+            height: 3.3vh;
+            width: 3.3vh;
+            position: absolute;
+            top: 0.4vh;
+            left: 1.6vh;
+            background-color: #fff;
+            border-radius: 45%;
+            z-index: -1;
+          }
+          .github-img{
+            width: 4vh;
+            height: 4vh;
+            }
+          p{
+            color: #fff;
+            font-size: 14px;
+            padding-top: 1vh;
+          }
+        }
+      }
+      .userImg{
+        img{
+          width: 5vh;
+          height: 5vh;
+          border-radius: 50%;
+        }
+        p{
+          color: #fff;
+          font-size: 17px;
+          padding-top: 1vh;
+        }
+      }
+      .log{
+        color: rgb(189, 189, 189);
+        font-size: 15px;
+        &:hover{
+          color: #fff;
+        }
+        span{
+          font-size: 19px;
+        }
+        p{
+          padding-top: 1vh;
+        }
+      }
+    }
+  }
+</style>
